df8e684517d4a4539f205daf38b731df4b85c982
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2023-04-06 21:43:36 +0900

    socket room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,48 +1,40 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
+// socket.io를 front-end와 연결
+const socket = io();
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
 }
 
-// socket : 서버로의 연결
-const socket = new WebSocket(`ws://${window.location.host}`);
+function showRoom() {
+  room.hidden = false;
+  welcome.hidden = true;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+}
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
-
-// socket.send()로 백엔드에서 보낸 내용 띄우기
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  console.log("socket.send    :", message.data);
-  messageList.append(li);
-});
-
-// 브라우저 연결이 끊기면 서버에게 알려줌
-socket.addEventListener("close", () => {
-  console.log("disconnected to Server ❌");
-});
-
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  // 백엔드로 보내기
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerText = `You: ${input.value}`;
-  messageList.append(li);
+  const input = form.querySelector("input");
+  // socket.send()로 메세지를 보내는게 아니라 "room" event로 emit해줌. 어떤 event든지 만들 수 있음(여기선 "enter_room"), object로 전송할 수 있음(ws와의 차이점)
+  // socket.emit -> 서버의 socket.on과 연결
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-}
+form.addEventListener("submit", handleRoomSubmit);
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,37 +12,19 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-// http, webSocket 둘 다 작동시키는 것. http 서버 위에 ws 서버를 만들기 http://~ ws://~ 요청을 둘 다 처리 할 수 있다.
-// ws 서버만 만들어도 됨. views , static 처리를 위해 둘 다 만듦
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-// socket : 연결된 어떤 사람,브라우저. 서버와 브라우저 간의 연결,  on : "" 이벤트를 기다림
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  // 서버가 오프라인이 되면 브라우저에게 알려줌
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser ❌");
+// done()을 실행하면 백엔드에서 실행되지 않음(보안상 문제), front-end의 함수를 실행시킨다
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
   });
-
-  // 브라우저가 서버에게 메세지를 보냈을 때 받기
-  socket.on("message", (message) => {
-    const parsedMsg = JSON.parse(message.toString("utf8"));
-    switch (parsedMsg.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${parsedMsg.payload}`)
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = parsedMsg.payload;
-        break;
-    }
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
   });
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
